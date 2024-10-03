450d2cdb9fb289cd3d0676a9b9b7644888193957
alya15062005 <152697440+alya15062005@users.noreply.github.com>
2024-10-03 16:03:21 +0700

    Update task.js
    -

--- task.js
+++ task.js
@@ -7,3 +7,7 @@ console.log(penjumlahan(1,5));
 //arrow function
 const input =  (y,i) => y + i;
 console.log (input(1,5));
+
+//perbedaan
+//function menggunakan kata kunci function memiliki sintaksis panjang.
+//arrow function fungsi yang sama namun lebih ringkas dan lebih banyak operator ternary.
