91913d6f53efa6a49338e2e2655972c9abca30c2
alya15062005 <152697440+alya15062005@users.noreply.github.com>
2024-10-03 15:00:05 +0700

    -
    -

---
+++ task.js
@@ -0,0 +1,9 @@
+//function 
+function penjumlahan (a,f){
+    return jumlah = a + f;
+}
+console.log(penjumlahan(1,5));
+
+//arrow function
+const input =  (y,i) => y + i;
+console.log (input(1,5));
